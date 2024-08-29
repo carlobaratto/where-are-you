194fe3001762e2c42a12e46a50d5ac4a4aafc127
Carlo Baratto <carlo.motocoder@gmail.com>
2024-08-29 16:32:20 +0200

    Update api_position.php

--- lib/PHPAPI/api_position.php
+++ lib/PHPAPI/api_position.php
@@ -13,45 +13,67 @@ if ($key=='IDDKFA'){
 
         unlink($name.".json");
         $myfile = fopen($name.".json", "w") or die("Unable to open file!");
-        $data = array(
-                        'name' => $name,
-                        'datetime' => $datetime, 
-                        'lat' => $lat, 
-                        'long' => $long
-                    );
 
-        $txt = json_encode($data);
+
+        $ParsedAry = [
+                
+                    "name" => $name, 
+                    "datetime" => $datetime, 
+                    'lat' => $lat, 
+                    'long' => $long
+                
+        ]; 
+ 
+ 
+/*
+        $position = array(
+            'datetime' => $datetime, 
+            'lat' => $lat, 
+            'long' => $long
+        );
+
+            $name = array(
+                'name' => $name,
+                'position' => $position
+            );
+*/
+        $txt = json_encode($ParsedAry);
         fwrite($myfile, $txt);
         fclose($myfile);
         echo "Chiamata riuscita";
     }
+
     if ($getset == 'get'){
+        $i=0;
+        $response = [];
         foreach (glob("*.json") as $fileinfo) {  //fileinfo contiene il filename
 
             $json = file_get_contents($fileinfo); 
 
-            
             // Check if the file was read successfully
             if ($json === false) {
                 die('Error reading the JSON file');
             }
 
-            // Decode the JSON file
-            $json_data .= $json; 
+            $obj = json_decode($json, TRUE);
 
             // Check if the JSON was decoded successfully
-            if ($json_data === null) {
+            if ($obj === null) {
                 die('Error decoding the JSON file');
             }
 
+            $response[$i] = $obj;
+            $i++;
         // Display data
-        echo $json_data;
-      
+    }
+    echo json_encode($response);
+//echo $response;
+//print_r($response);
     }
 }
 else {
         echo "Wrong api key";
      }
-}
+
 ?>
 
